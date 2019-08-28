040160e823c2c442f65ad05f68246a1146840dcb
Jonathan Payne <jonathan.b.payne@gmail.com>
2019-08-28 06:49:18 -0400

    modified PersonService to return observable and delay

--- Lab2/src/app/newer/newer.component.ts
+++ Lab2/src/app/newer/newer.component.ts
@@ -14,7 +14,11 @@ export class NewerComponent implements OnInit {
 
   // Use the ngOnInit() method to get the Person from the service.
   ngOnInit() {
-    this.person = this.personService.getPerson(this.personService.personA);
+    // this.person = this.personService.getPerson(this.personService.personA);
+ 
+    // Change the call in the component to subscribe() to the call rather than being a synchronous call.
+    // call the method to delay before returning the value:
+    this.personService.delay(4000).then(() => this.personService.getPerson(this.personService.personA).subscribe(person => this.person = person));
     }
 
   // Create a member variable of type Person
--- Lab2/src/app/person.service.ts
+++ Lab2/src/app/person.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 // In the Service class, reference the Person class from above. 
 import { Person } from './Person';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,8 +17,22 @@ export class PersonService {
     this.personA = new Person("Abe", "Froman");
    }
 
-  // Next, create a basic method to return the person such as getPerson()
-  getPerson(person: Person): Person {
-    return person;
-  }
+// Next, create a basic method to return the person such as getPerson()
+/**  getPerson(person: Person): Person {
+*    return person;
+*  }
+*/
+
+// To really see the process of Observables without a long web delay, 
+// create this function inside the service class:
+
+async delay(ms: number) {
+await new Promise(resolve => setTimeout(()=>resolve(), ms));
+}
+
+// Change the service call to return an Obervable rather than a Person object.
+getPerson(person: Person): Observable<Person> {
+  return of(person);
+}
+
 }
